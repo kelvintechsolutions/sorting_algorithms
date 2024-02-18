5c9146b2df367fe754d01bda9aff29c2452b7b72
kelvintechsolutions <kevohmutugi51@gmail.com>
2024-02-18 03:59:45 -0800

    task add

---
+++ 0-bubble_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+ * swap_bubble -it functions for sorting with swap method.
+ *@a:it is the element to be swapped.
+ *@b:it is the element to be swapped.
+ * Return:it returns nothing.
+ */
+
+void swap_bubble(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort -it sorts an array using bubble sort algorithm.
+ *@array: array to be sorted.
+ *@size:it is the size to be sorted.
+ * Return:it is  void.
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, sz = size;
+	bool booln = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (booln == false)
+	{
+		booln = true;
+		for (i = 0; i < sz - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_bubble(array + i, array + i + 1);
+				print_array(array, size);
+				booln = false;
+			}
+		}
+		sz--;
+	}
+}
